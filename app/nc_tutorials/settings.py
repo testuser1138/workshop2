21d77c542ae7ac3314e5907599df91d1d16a6ca1
Mac McCarty <git@notmyrealemail.net>
2021-11-06 07:46:15 -0500

    Sat Nov  6 07:46:15 CDT 2021 macm | devops week 2 - workshop2 checkpoint

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 DEBUG = True
 
 ALLOWED_HOSTS = [
-  'ec2-3-14-230-216.us-east-2.compute.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1'
+  #'ec2-3-14-230-216.us-east-2.compute.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1'
+  'ec2-54-172-161-26.compute-1.amazonaws.com', '0.0.0.0', 'localhost', '127.0.0.1'
 ]
 
 
@@ -129,7 +130,7 @@ USE_TZ = True
 if DEBUG:
   STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 else:
-  STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]  
+  STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_URL = '/static/'
 
